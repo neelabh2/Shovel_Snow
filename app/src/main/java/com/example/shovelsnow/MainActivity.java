0814d82620259968119a69e1e00ff80a106430e7
neel2 <neelabh2@illinois.edu>
2019-11-28 13:45:15 -0600

    Added a game file

--- app/src/main/java/com/example/shovelsnow/MainActivity.java
+++ app/src/main/java/com/example/shovelsnow/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.shovelsnow;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,5 +14,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView titleText = findViewById(R.id.titleScreenText);
+        Button optionsButton = findViewById(R.id.optionsButton);
+        Button startButton = findViewById(R.id.playButton);
+
     }
 }
