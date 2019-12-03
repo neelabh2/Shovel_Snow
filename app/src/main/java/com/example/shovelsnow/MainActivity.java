1b2688c9275bbf80546520aca2ee07fe791773e2
neel2 <neelabh2@illinois.edu>
2019-12-03 13:33:54 -0600

    Merge branch 'master' of C:\Users\neel2\AndroidStudioProjects\ShovelSnow with conflicts.

--- app/src/main/java/com/example/shovelsnow/MainActivity.java
+++ app/src/main/java/com/example/shovelsnow/MainActivity.java
@@ -8,6 +8,12 @@ import android.os.Bundle;
 import android.widget.Button;
 
 /**
+ * MAKE SURE TO CHANGE THE XML FILE TO THE PROPER BACKGROUND IMAGE
+ * might also want to try with an ImageView - maybe we can change that on the fly?
+ * otherwise we're stuck with one image. Also not sure if that'll be in the background.
+ */
+
+/**
  * App launches to this activity.
  * Displays the app title and a button for navigating to GameActivity.
  */
@@ -36,16 +42,16 @@ public class MainActivity extends AppCompatActivity {
         optionsButton.setOnClickListener(unused -> optionsButtonClicked());
 
         //not sure why I can't reference R.raw
-        //titleMusic = MediaPlayer.create(this, R.raw.bensound-thelounge.mp3);
-        //titleMusic.start();
+        titleMusic = MediaPlayer.create(this, R.raw.bensound_thelounge);
+        titleMusic.start();
     }
 
     /**
      * Starts GameActivity when startButton is clicked.
      */
     private void startButtonClicked() {
-        //titleMusic.release();
-        //titleMusic = null;
+        titleMusic.release();
+        titleMusic = null;
         startActivity(new Intent(this, GameActivity.class));
     }
 
@@ -53,8 +59,8 @@ public class MainActivity extends AppCompatActivity {
      * Starts optionsActivity when optionsButton is clicked.
      */
     private void optionsButtonClicked() {
-        //titleMusic.release();
-        //titleMusic = null;
+        titleMusic.release();
+        titleMusic = null;
         startActivity(new Intent(this, OptionsActivity.class));
     }
 }
